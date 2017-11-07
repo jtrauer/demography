a4dd720a71cec6bf7afb0dcbd2d7a7b9e4a35d7a
jtrauer <james.trauer@monash.edu>
2017-11-07 15:00:47 +1100

    Read all GRIM sheets

--- grim_reader.py
+++ grim_reader.py
@@ -2,6 +2,7 @@
 from xlrd import open_workbook
 import numpy
 
+
 def remove_element_from_unicode(unicode_string, element_number, insertion_element):
     """
     Removes a specific unicode character from a unicode string and replaces it with a unicode string.
@@ -40,73 +41,128 @@ def convert_to_integer_if_possible(values_list):
     return values_found
 
 
-# open excel workbook and find deaths sheet
-book = open_workbook('grim-all-causes-combined-2017.xlsx')
-sheet = book.sheet_by_name('Deaths')
+def read_grim_sheet(workbook, years_to_keep=None):
 
-# initialise
-data_type = []
-title_row_index = 5
-titles = {}
-working_array = {}
-gender = 'start'
-new_layer = False
-columns_to_ignore = ['', 'Total']
+    # initialise
+    sheet = workbook.sheet_by_name('Deaths')
+    data_type = []
+    title_row_index = 5
+    titles = {}
+    working_array = {}
+    gender = 'start'
+    new_layer = False
+    columns_to_ignore = ['', 'Total']
 
-for c in range(sheet.ncols):
+    for c in range(sheet.ncols):
 
-    # update first level of dictionary indices if necessary, the "data type"
-    if sheet.col_values(c)[3] != u'':
-        gender = str(sheet.col_values(c)[3])
-        new_layer = True
-    data_type.append(gender)
+        # update first level of dictionary indices if necessary, the "data type"
+        if sheet.col_values(c)[3] != u'':
+            gender = str(sheet.col_values(c)[3])
+            new_layer = True
+        data_type.append(gender)
 
-    # find second level of dictionary indices, i.e. the "title"
-    title = remove_element_from_unicode(sheet.col_values(c)[title_row_index], 8211, u'to')
+        # find second level of dictionary indices, i.e. the "title"
+        title = remove_element_from_unicode(sheet.col_values(c)[title_row_index], 8211, u'to')
 
-    # process column values
-    column_values = convert_to_integer_if_possible(sheet.col_values(c)[title_row_index:])
+        # process column values
+        column_values = convert_to_integer_if_possible(sheet.col_values(c)[title_row_index:])
 
-    # record year values
-    if 'Year' in title:
-        years = column_values
+        # record year values
+        if 'Year' in title:
+            years = column_values
 
-    # ignore columns with no data
-    elif title in columns_to_ignore:
-        pass
+        # ignore columns with no data
+        elif title in columns_to_ignore:
+            pass
 
-    # create main data structure
-    elif new_layer:
-        working_array[gender] = numpy.array(column_values)
-        new_layer = False
-        titles[gender] = [title]
-    elif gender != 'start':
-        working_array[gender] = numpy.vstack((working_array[gender], numpy.array(column_values)))
-        titles[gender].append(title)
+        # create main data structure
+        elif new_layer:
+            working_array[gender] = numpy.array(column_values)
+            new_layer = False
+            titles[gender] = [title]
+        elif gender != 'start':
+            working_array[gender] = numpy.vstack((working_array[gender], numpy.array(column_values)))
+            titles[gender].append(title)
 
-# depth stack the arrays created above
-age_groups = titles['Persons']
-layer_titles = []
-final_array = numpy.array(numpy.zeros(shape=(working_array['Persons'].shape[0], working_array['Persons'].shape[1], 0)))
-for gender in working_array:
-    final_array = numpy.dstack((final_array, working_array[gender]))
-    layer_titles.append(gender)
+    # depth stack the arrays created above
+    age_groups = titles['Persons']
+    genders = []
+    final_array = numpy.array(numpy.zeros(shape=(working_array['Persons'].shape[0], working_array['Persons'].shape[1], 0)))
+    for gender in working_array:
+        final_array = numpy.dstack((final_array, working_array[gender]))
+        genders.append(gender)
 
-# if all data is zeros for that row across all layers, discard that row (year)
-years_to_keep = numpy.any(numpy.all(final_array, axis=0), axis=1)
-final_array = final_array[:, years_to_keep, :]
-years = list(numpy.array(years)[years_to_keep])
+    # if all data is zeros for that row across all layers, discard that row (year)
+    if years_to_keep is None: years_to_keep = numpy.any(numpy.all(final_array, axis=0), axis=1)
+    final_array = final_array[:, years_to_keep, :]
+    years = list(numpy.array(years)[years_to_keep])
 
-# first dimension is age groups, second is years, third is gender for the final_array
-
+    return age_groups, years, genders, final_array, years_to_keep
 
 
+def read_all_grim_sheets(sheet_names):
+    """
+    Master function loop over all sheets and read each one, then concatenate the sheets together along the fourth
+    dimension.
 
+    Args:
+        sheet_names: The sheets that need to be read
+    Returns:
+         age_groups: Age group strings directly from the sheet reading function
+         years: List of years as integers directly from teh sheet reading function
+         final_array: The final data structure in four dimensions by age group, years, gender and sheet (cause of death)
+    """
 
-# code for pandas (incomplete and not working as it should - have abandoned for now)
-# import pandas
-# grim_all_causes_file = pandas.ExcelFile('grim-all-causes-combined-2017.xlsx')
-# grim_all_causes = {'male': grim_all_causes_file.parse('Deaths', header=5, usecols=range(0, 21))}
-# grim_all_causes['male'].fillna(0, inplace=True)
+    # loop through sheet names
+    for name in sheet_names:
 
+        # ensure we keep the years from the all causes sheet, as there are several years with zero data in other sheets
+        if name == sheet_names[0]:
+            years_to_keep = None
+        else:
+            years_to_keep = years_to_keep
+
+        # open excel workbook and find deaths sheet
+        book = open_workbook('grim-' + name + '-2017.xlsx')
+
+        # read with reading function above
+        age_groups, years, genders, sheet_array, years_to_keep = read_grim_sheet(book, years_to_keep=years_to_keep)
+        if name == sheet_names[0]: final_array = numpy.array(numpy.zeros(shape=list(sheet_array.shape) + [0L]))
+        sheet_array = numpy.expand_dims(sheet_array, axis=3)
+
+        # first dimension is age groups, second is years, third is gender, fourth is cause of death
+        final_array = numpy.concatenate((final_array, sheet_array), axis=3)
+
+    return age_groups, years, genders, final_array
+
+if __name__ == '__main__':
+
+    # specify spreadsheets to read and read them into single data structure - always put all-causes-combined first
+    sheet_names = ['all-causes-combined',
+                   'all-certain-conditions-originating-in-the-perinatal-period',
+                   'all-certain-infectious-and-parasitic-diseases',
+                   'all-diseases-of-the-circulatory-system',
+                   'all-congenital-malformations-deformations-and-chromosomal-abnormalities',
+                   'all-diseases-of-the-blood-and-blood-forming-organs',
+                   'all-diseases-of-the-digestive-system',
+                   'all-diseases-of-the-ear-and-mastoid-process',
+                   'all-diseases-of-the-eye-and-adnexa',
+                   'all-diseases-of-the-genitourinary-system',
+                   'all-diseases-of-the-musculoskeletal-system-and-connective-tissue',
+                   'all-diseases-of-the-nervous-system',
+                   'all-diseases-of-the-respiratory-system',
+                   'all-diseases-of-the-skin-and-subcutaneous-tissue',
+                   'all-endocrine-nutritional-and-metabolic-diseases',
+                   'all-external-causes-of-morbidity-and-mortality',
+                   'all-mental-and-behavioural-disorders',
+                   'all-neoplasms',
+                   'all-pregnancy-childbirth-and-the-puerperium',
+                   'all-symptoms-signs-and-abnormal-clinical-and-laboratory-findings-not-elsewhere-classified',
+                   'asthma', 'breast-cancer', 'chronic-kidney-disease', 'colorectal-cancer',
+                   'chronic-obstructive-pulmonary-disease', 'coronary-heart-disease', 'diabetes', 'heart-failure',
+                   'lung-cancer', 'melanoma', 'osteoarthritis', 'osteoporosis', 'prostate-cancer', 'rheumatoid-arthritis',
+                   'stroke', 'cerebrovascular-disease', 'dementia-and-alzheimer-disease', 'hypertensive-disease',
+                   'kidney-failure', 'suicide', 'accidental-drowning', 'accidental-poisoning', 'assault',
+                   'land-transport-accidents', 'liver-disease']
+    age_groups, years, genders, final_array = read_all_grim_sheets(sheet_names)
 
